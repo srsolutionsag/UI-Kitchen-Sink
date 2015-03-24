2ffe6ffac446a21fb9fa539e84147c6297ba9ed5
Timon Amstutz <timon.amstutz@bluewin.ch>
2015-03-24 12:12:38 +0100

    Also display code in view

--- app/js/directives/entry/entryContentDirective.js
+++ app/js/directives/entry/entryContentDirective.js
@@ -1,4 +1,4 @@
-module.directive('entryContent', function () {
+module.directive('entryContent', function ($http) {
     return {
         restrict: 'AEC',
         scope: {
@@ -10,6 +10,18 @@ module.directive('entryContent', function () {
         replace: true,
         link: function (scope, element) {
             scope.path = scope.tabPath+'/'+scope.entry.id+'.html';
+            $http.get(scope.path)
+                .success(function(data) {
+                    if(data.indexOf("<span ng-non-bindable>")>-1){
+                        data = data.replace("<span ng-non-bindable>", "");
+                        data =data.slice(0,data.length-7)
+                    }
+                    scope.htmlCode = data;
+                    console.log(scope.htmlCode);
+                })
+                .error(function() {
+                    console.log('could not find '+scope.path);
+                });
         }
     };
 });
