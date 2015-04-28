262e17980ecc6aa5caeefe2a154e9f231f60a083
Timon Amstutz <timon.amstutz@bluewin.ch>
2015-04-28 18:28:41 +0200

    Adaptions towards ilias

--- app/js/controllers/headerController.js
+++ app/js/controllers/headerController.js
@@ -1,10 +1,12 @@
 angular.module('uiKitchenSink').controller('HeaderController', function($scope, Entries) {
-
-    $scope.onDropdownEntrySelected = function (categoryIndex,subCategoryIndex) {
-        Entries.changeSelection(categoryIndex,subCategoryIndex,0);
-    };
+    $scope.categorySelected = {};
+    $scope.categorySelected.index = -1;
 
     $scope.$on('changeSelection', function (d,categoryIndex,subCategoryIndex,tabIndex) {
+        if(typeof $scope.categories === 'undefined'){
+            $scope.categories = Entries.categories;
+
+        }
         if(categoryIndex!=-1){
             $scope.categorySelected = {"index":categoryIndex,"title":$scope.categories[categoryIndex].title,"id":$scope.categories[categoryIndex].id};
             $scope.subCategorySelected = $scope.categories[categoryIndex].subCategories[subCategoryIndex];
@@ -15,10 +17,8 @@ angular.module('uiKitchenSink').controller('HeaderController', function($scope, 
             $scope.categorySelected.index = -1;
         }
     });
+
     Entries.promisedData().then(function(){
-        $scope.categories = Entries.categories;
-        $scope.categorySelected ={};
-        $scope.categorySelected.index = -1;
+        $scope.searchCategories = Entries.categories;
     });
-    $scope.onDropdownEntrySelected(-1,0);
 });
--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,4 +1,21 @@
 angular.module('uiKitchenSink').controller('MainController', function($scope,  $routeParams, Entries) {
+    Entries.promisedData().then(function(){
+        $scope.doRouting();
+    });
+
+    $scope.doRouting = function(){
+        if(!$routeParams.categoryIndex){
+            $routeParams.categoryIndex = 0;
+        }
+        if(!$routeParams.subCategoryIndex){
+            $routeParams.subCategoryIndex = 0;
+        }
+        if(!$routeParams.itemGroupIndex){
+            $routeParams.itemGroupIndex = 0;
+        }
+        Entries.changeSelection($routeParams.categoryIndex,$routeParams.subCategoryIndex,$routeParams.itemGroupIndex);
+    };
+
     $scope.onAreaSelected = function (area) {
         console.log(area);
     };
