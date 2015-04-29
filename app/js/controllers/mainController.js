bf8ce30b42eb5e5d011cbb8654187322cf4443a1
Timon Amstutz <timon.amstutz@bluewin.ch>
2015-04-29 13:18:24 +0200

    Columns update

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,4 +1,46 @@
 angular.module('uiKitchenSink').controller('MainController', function($scope,  $routeParams, Entries) {
+    $scope.contentClasses = {};
+    $scope.contentClasses.full = {};
+    $scope.contentClasses.three = {};
+    $scope.contentClasses.left = {};
+    $scope.contentClasses.right = {};
+
+    $scope.contentClasses.full.left = "hide";
+    $scope.contentClasses.full.center = "col-sm-12";
+    $scope.contentClasses.full.right = "hide";
+
+    $scope.contentClasses.left.left = "col-sm-4 il-sidebar-left";
+    $scope.contentClasses.left.center = "col-sm-8 col-sm-offset-4";
+    $scope.contentClasses.left.right = "hide";
+
+    $scope.contentClasses.right.left = "hide";
+    $scope.contentClasses.right.center = "col-sm-8";
+    $scope.contentClasses.right.right = "col-sm-4 il-sidebar-right";
+
+    $scope.contentClasses.three.left = "col-sm-3 il-sidebar-left";
+    $scope.contentClasses.three.center = "col-sm-6 col-sm-offset-3";
+    $scope.contentClasses.three.right = "col-sm-3 il-sidebar-right";
+
+    $scope.contentClasses.current = $scope.contentClasses.full;
+
+    $scope.onViewChange = function(type){
+        switch(type){
+            case "full":
+                $scope.contentClasses.current = $scope.contentClasses.full;
+                break;
+            case "left":
+                $scope.contentClasses.current = $scope.contentClasses.left;
+                break;
+            case "right":
+                $scope.contentClasses.current = $scope.contentClasses.right;
+                break;
+            case "three":
+                $scope.contentClasses.current = $scope.contentClasses.three;
+                break;
+
+        }
+
+    };
     Entries.promisedData().then(function(){
         $scope.doRouting();
     });
@@ -20,3 +62,6 @@ angular.module('uiKitchenSink').controller('MainController', function($scope,  $
         console.log(area);
     };
 });
+
+
+
